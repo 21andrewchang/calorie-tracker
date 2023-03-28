1de89868bc44e45fce13589e0a75c6e9282eb381
Andrew Chang <andrew51@illinois.edu>
2023-03-28 08:45:35 -0500

    Initial Commit

---
+++ CalorieTracker/CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by Andrew Chang on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
